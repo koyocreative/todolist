36d9f81660eba44720ec56935241030e57c912d0
Aleksandar Kojovic <koyocreative@gmail.com>
2020-02-29 19:22:31 +0100

    working on drag and drop

--- js/script.js
+++ js/script.js
@@ -4,19 +4,24 @@ const todoBtn = document.querySelector('.todo-add')
 
 const list = [
     {
-        name: 'Buy Milk'
+        name: 'Buy Milk',
+        status: 'done'
     },
     {
-        name: 'Get Toalet Paper'
+        name: 'Get Toalet Paper',
+        status: ''
     }
 ]
 
 
 // Create Template Function
-const todoTmpl = ( id, title ) => {
+const todoTmpl = ( id, title, status = '' ) => {
     return html = `
-        <div class="todo-item">
-            ${title}<span data-id="${id}" class="close">✕</span>
+        <div data-id="${id}" class="todo-item ${status}" draggable="true">
+            
+            <h2>${title}</h2>
+            <span class="close">✕</span>
+
         </div>
 
     `;
@@ -28,14 +33,16 @@ const todoFactory = (todoz) => {
     let counter = 0;
     todoz.forEach( (data) => {
         // console.log(data)
-        todoList.innerHTML += todoTmpl( counter, data.name)
+        todoList.innerHTML += todoTmpl( counter, data.name, data.status)
         counter++;
     })
+    Dragging();
+    checkListener();
     removeListener();
 }
 
 const addTodo = (name) => {
-    list.push({ name });
+    list.push({ name, status: '' });
 }
 
 const clear = () => {
@@ -46,13 +53,31 @@ const removeListener = () => {
     let items = document.querySelectorAll('.close')
     items.forEach( (item) => {
         item.addEventListener('click', function() {
-            this.parentNode.remove()
-            list.splice( this.dataset.id, 1);
+            this.parentNode.classList.add('remove')
+            setTimeout( () => {
+                this.parentNode.remove()
+                list.splice( this.parentNode.dataset.id, 1);
 
-            todoFactory(list)
+                todoFactory(list)
+            }, 200)
+
         })
     })
 }
+
+const checkListener = () => {
+    let items = document.querySelectorAll('.todo-item h2');
+    items.forEach( (item) => {
+        item.addEventListener('click', function() {
+            // toggle .done on .todo-item
+            this.parentNode.classList.contains('done') ? this.parentNode.classList.remove('done') : this.parentNode.classList.add('done')
+            // toggle status in the list obj
+            list[this.parentNode.dataset.id].status == 'done' ? list[this.parentNode.dataset.id].status = '' : list[this.parentNode.dataset.id].status = 'done'
+        })
+    })
+}
+
+
 
 // Function for adding event listeners on todo-item
 todoBtn.addEventListener('click', function() {
@@ -60,5 +85,61 @@ todoBtn.addEventListener('click', function() {
     todoFactory(list)
     todoInput.value = ''
 })
+
+
+// Drag and Drop
+const Dragging = () => {
+
+    let dropList = document.querySelectorAll('.todo-item');
+    console.log(dropList);
+
+    let draggedItem = '';
+
+    for( let d = 0; d < dropList.length; d++ ) {
+
+        const item = dropList[d];
+
+        // dragstart
+        item.addEventListener('dragstart', function(e) {
+            draggedItem = this
+            console.log(draggedItem)
+
+            setTimeout( function() {
+                item.style.opacity = 0
+            }, 0 )
+        })
+
+        // dragend
+        item.addEventListener('dragend', function() {
+            
+            setTimeout( function() {
+                draggedItem.style.opacity = 1
+                draggedItem = ''
+            }, 0)
+        })
+
+    }
+
+    todoList.addEventListener('dragover', function(e) {
+        e.preventDefault()
+        console.log('dragover')
+    })
+    todoList.addEventListener('dragenter', function(e) {
+        e.preventDefault()
+        console.log('dragenter')
+    })
+    todoList.addEventListener('dragleave', function(e) {
+        //...
+        e.preventDefault()
+        console.log('dragleave')
+    })
+    todoList.addEventListener('drop', function(e) {
+        console.log(this)
+        this.append(draggedItem)
+    })
+}
+
+
+
 
 todoFactory(list)
